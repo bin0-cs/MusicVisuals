297924b2709e5d8ddcdd76f0e9d65fb0293edbf1
bin0-cs <oltean444@gmail.com>
2021-05-05 17:30:32 +0100

    update

--- java/src/c19383843/MyVisuals.java
+++ java/src/c19383843/MyVisuals.java
@@ -10,5 +10,46 @@ import ddf.minim.analysis.FFT;
 import processing.core.PFont;
 
 public class MyVisuals extends Visual{
-    
+
+//Visuals 
+YinY yiny;
+
+
+Minim minim; // Connect to minim
+AudioInput ai; // How to connect to mic
+AudioPlayer ap;
+AudioBuffer ab; // Samples
+FFT fft;
+float[] lerpedBuffer;
+float y = 200;
+float lerpedY = y;
+int which = 0;
+
+
+float halfHeight = height / 2;
+float average = getAmplitude();
+float sum = 0;
+float lerpedAverage = 0;
+
+public void keyPressed() {
+    if (keyCode >= '0' && keyCode <= '6') {
+        which = keyCode - '0';
+    }
+    if (keyCode == ' ') {
+        if (ap.isPlaying()) {
+            ap.pause();
+        } else {
+            ap.rewind();
+            ap.play();
+        }
+    }
+}
+public void settings()
+{
+    // size(600, 600, P3D);
+    fullScreen(P3D, 1);
+}
+
+
+
 }
---
+++ java/src/c19383843/Render.java
@@ -0,0 +1,7 @@
+package c19383843;
+
+public interface Render {
+    public void render();
+    public void update();
+    
+}
